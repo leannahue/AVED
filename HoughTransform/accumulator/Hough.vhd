3c5d53e532f75cce41f901ee75db464a0db4e299
Leanna Hue <leannahue2020@u.northwestern.edu>
2019-05-30 16:55:01 -0500

    modified accumulator vhdl

--- HoughTransform/accumulator/Hough.vhd
+++ HoughTransform/accumulator/Hough.vhd
@@ -19,11 +19,11 @@ port
 (
 	signal clock         : in  std_logic;
     	signal reset         : in  std_logic;
-	signal in_rd_en      : out std_logic; 
-	signal in_empty      : in  std_logic; 
-	signal in_dout       : in  std_logic_vector(7 downto 0); 
-	signal out_wr_en     : out std_logic; 
-	signal out_full      : in  std_logic; 
+	signal in_rd_en      : out std_logic;
+	signal in_empty      : in  std_logic;
+	signal in_dout       : in  std_logic_vector(7 downto 0);
+	signal out_wr_en     : out std_logic;
+	signal out_full      : in  std_logic;
 	signal out_din       : out std_logic_vector(7 downto 0)
 );
 end entity Hough;
@@ -31,10 +31,10 @@ end entity Hough;
 architecture behavior of Hough is
 
 	 type accumulator is array (natural range <> ) of integer;
-	 --signal accu_h   	: integer := 1018; 
+	 --signal accu_h   	: integer := 1018;
 	 --signal accu_w   	: integer := 180;
 	 signal center_x        : integer := WIDTH/2;
-	 signal center_y        : integer := HEIGHT/2;  
+	 signal center_y        : integer := HEIGHT/2;
 	 signal x, x_c       		: integer;
    	 signal y, y_c       		: integer;
 	 signal threshold  		: integer := 50;
@@ -74,6 +74,8 @@ function quantize_n( val : integer; n : integer )
         return temp;
     end function;
 
+		signal cordic_in_rd_en : std_logic;
+
 begin
 
 cordic_inst : cordic
@@ -81,7 +83,7 @@ cordic_inst : cordic
         clock => clock,
         reset => reset,
         in_dout => theta_rd,
-        in_rd_en => in_rd_en,
+        in_rd_en => cordic_in_rd_en,
         in_empty => in_empty,
         out_cos_wr_en => out_cos_wr_en,
         out_cos_din => cos_theta,
@@ -92,7 +94,7 @@ cordic_inst : cordic
     );
 
 
-    Accumulate_process : process( state, in_dout, x, y) is
+    Accumulate_process : process( state, in_dout, x, y, in_empty, out_full, cordic_in_rd_en, out_cos_wr_en, out_sin_wr_en ) is
 	variable r, hough_h, temp : signed (47 downto 0);
       	variable x_position, y_position : signed (15 downto 0);
     begin
@@ -102,8 +104,8 @@ cordic_inst : cordic
 	accu_array_c <= accu_array;
 	in_rd_en <= '0';
         out_wr_en <= '0';
-        out_din <= (others => '0');	
-        
+        out_din <= (others => '0');
+
         case ( state ) is
             when s0 =>
                 x_c <= 0;
@@ -114,11 +116,11 @@ cordic_inst : cordic
             when s1 =>
                 if ( in_empty = '0' ) then
                     in_rd_en <= '1';
-		    pixel <= in_dout; 
-		if (to_integer(unsigned(pixel)) > threshold) then
+		    pixel <= in_dout;
+					x_c <= x + 1;
+		if (to_integer(unsigned(in_dout)) > threshold) then
 			next_state <= s2;
 		else
-			x_c <= x + 1;
 			next_state <= s1;
 		end if;
   		if ( x = WIDTH - 1 ) then
@@ -127,25 +129,27 @@ cordic_inst : cordic
                 end if;
                 if ( (x = (WIDTH -1)) AND (y = (HEIGHT -1)) )then
                         x_c <= 0;
-                        y_c <= 0; 
+                        y_c <= 0;
 			next_state <= s3;
-		end if; 
+		end if;
                 end if;
             when s2 =>
 		in_rd_en <= '0';
+		if (cordic_in_rd_en = '1') then
 		theta_rd <=  std_logic_vector(resize(sra_func(((((quantize_n(theta, 14) sll 14) + quantize_n(180, 13)) / quantize_n(180, 14)) * to_signed(PI, 64)) + quantize_n(1, 13), 14), 32));
-		--just followed how to give input to cordic component in cordic_tb file 
+		end if;
+		--just followed how to give input to cordic component in cordic_tb file
 		  if ((out_cos_wr_en = '1') AND (out_sin_wr_en = '1')) then
-			
-			
+
+
 			x_position :=  to_signed((x - (WIDTH/2)), 16);
-			y_position :=  to_signed((y - (HEIGHT/2)), 16); 
+			y_position :=  to_signed((y - (HEIGHT/2)), 16);
 			r := x_position*signed(cos_theta) + y_position*signed(sin_theta);
 			hough_h := to_signed(sqrt_2, 32)*to_signed((WIDTH/2),16);
 			temp:= (hough_h + r) srl 16;
 			accu_array_c(to_integer(temp)*accu_w + theta) <= accu_array(to_integer(temp)*accu_w + theta) + 1;
 		 	theta_c <= theta + 1;
- 		  end if;
+
 		if (theta = 180) then
 			theta_c <= 0;
 			x_c <= x + 1;
@@ -156,10 +160,11 @@ cordic_inst : cordic
                     end if;
                     if ( (x = (WIDTH -1)) AND (y = (HEIGHT -1)) )then
                         x_c <= 0;
-                        y_c <= 0; 
-			next_state <= s3;  
+                        y_c <= 0;
+			next_state <= s3;
                     end if;
 		end if;
+		 end if;
             when s3 =>
 		--output the accumulator
 		out_wr_en <= '1';
@@ -180,8 +185,8 @@ cordic_inst : cordic
                 next_state <= s0;
         end case;
     end process Accumulate_process;
-    
-    
+
+
     clock_process : process( clock, reset ) is
     begin
         if ( reset = '1' ) then
@@ -189,7 +194,7 @@ cordic_inst : cordic
             x <= 0;
             y <= 0;
 	    theta<= 0;
-	    accu_array <= (others => 0);       
+	    accu_array <= (others => 0);
         elsif ( rising_edge(clock) ) then
             state <= next_state;
             x <= x_c;
