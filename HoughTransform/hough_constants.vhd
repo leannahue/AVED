3d14c87b9455db2b5888bd3ccdd25f0fdde43f63
Leanna Hue <leannahue2020@u.northwestern.edu>
2019-06-06 11:58:47 -0500

    added finite lines, draw lines, changed threshold

--- HoughTransform/hough_constants.vhd
+++ HoughTransform/hough_constants.vhd
@@ -6,30 +6,169 @@ use STD.textio.all;
 
 package hough_constants is
 
-	constant IMG_WIDTH	: natural := 720;
-	constant IMG_HEIGHT	: natural := 720;
-	constant IMG_SIZE	: natural := IMG_WIDTH*IMG_HEIGHT;
-	constant MAG_WIDTH	: natural := 8;
-	constant RHO_WIDTH	: natural := 10;
-	constant THETA_WIDTH	: natural := 8;
+	constant IMG_WIDTH			: natural := 720;
+	constant IMG_HEIGHT			: natural := 720;
+	constant IMG_SIZE				: natural := IMG_WIDTH*IMG_HEIGHT;
+	constant MAG_WIDTH			: natural := 8;
+	constant RHO_WIDTH			: natural := 10;
+	constant THETA_WIDTH		: natural := 8;
 
-	constant ACCU_WIDTH	: natural := 180;
-	constant ACCU_HEIGHT	: natural := 1018;  -- sqrt(2)*IMG_WIDTH + sqrt(2)*IMG_HEIGHT
+	constant W_div2         : integer := IMG_WIDTH / 2;
+	constant H_div2         : integer := IMG_HEIGHT / 2; --sqrt(2)*720
 
+	constant ACCU_WIDTH			: natural := 180;
+	constant ACCU_HEIGHT		: natural := 1018;  -- sqrt(2)*IMG_WIDTH + sqrt(2)*IMG_HEIGHT
+
+	constant ACCU_h_div2    : integer := ACCU_HEIGHT / 2;
+
+	constant SQRT_2					: natural := 92682; --sqrt(2)*2^16
+
+	-- int hough_h = sqrt2_quant * ((height > width? height : width) / 2.0);
+	constant HOUGH_H				: natural := 509*2**14;
+
+	constant IMG_THRESHOLD  : natural := 70;
 	constant ACCU_THRESHOLD : natural := 110;
 
-component hough_threshold is
-	port (
-		signal clock		: in std_logic;
-		signal reset		: in std_logic;
-		signal in_rd_en		: out std_logic;
-		signal in_empty		: in std_logic;
-		signal in_dout		: in std_logic_vector (MAG_WIDTH - 1 downto 0); -- accumulator input
-		signal out_wr_en	: out std_logic;
-		signal out_full		: in std_logic;
-		signal out_rho	        : out std_logic_vector (RHO_WIDTH - 1 downto 0);
-		signal out_theta	: out std_logic_vector (THETA_WIDTH - 1 downto 0)
+	constant PI_div_180  		: integer := 1143;  -- PI/180*2^16
+
+	constant XY_D_WIDTH	: natural := RHO_WIDTH*4;  -- x1,x2,y1,y2
+	constant XY_BUFF_SIZE	: natural := 32;
+
+	constant FIFO_D_WIDTH	: natural := 8;
+	constant FIFO_BUFF_SIZE	: natural := 32;
+
+	-- Cordic Constants
+	constant N_STAGES : integer := 16;
+	constant DATA_LENGTH : integer := 721;
+	constant K : std_logic_vector (31 downto 0) := x"000026dd";
+	constant PI : integer := 51472;
+	constant NEG_PI : integer := -51472;
+	constant TWO_PI : integer := 102944;
+	constant PI_OVER_TWO : integer := 25736;
+	constant NEG_PI_OVER_TWO : integer := -25736;
+
+	-- Main components
+
+	component hough_top is
+		generic (
+		    constant WIDTH   : integer:= IMG_WIDTH;
+		    constant HEIGHT  : integer:= IMG_HEIGHT
 		);
-end component hough_threshold;
+		port (
+			signal clock     : in std_logic;
+			signal reset     : in std_logic;
+			signal in_full   : out std_logic;
+			signal in_wr_en  : in std_logic;
+			signal in_din    : in std_logic_vector (7 downto 0);
+			signal out_rd_en : in std_logic;
+			signal out_empty : out std_logic;
+			signal out_dout  : out std_logic_vector (7 downto 0)
+		);
+	end component hough_top;
+
+	-- ACCUMULATOR GOES HERE
+
+	component threshold is
+	  port (
+	    signal clock	       	: in std_logic;
+	    signal reset	       	: in std_logic;
+	    signal in_rd_en	    	: out std_logic;
+	    signal in_empty		    : in std_logic;
+	    signal out_full		    : in std_logic;
+	    signal in_dout	     	: in std_logic_vector (MAG_WIDTH - 1 downto 0); -- accumulator input
+	    signal out_wr_en     	: out std_logic;  -- '1' when out_rho and out_theta are valid
+	    signal out_rho_done	  : out std_logic;  -- rho/theta calculation done
+	    signal out_rho	      : out std_logic_vector (RHO_WIDTH - 1 downto 0);
+	    signal out_theta	    : out std_logic_vector (THETA_WIDTH - 1 downto 0)
+	  );
+	end component threshold;
+
+	component finite_lines is
+	  port (
+		  signal clock         : in  std_logic;
+		  signal reset         : in  std_logic;
+		  signal in_empty      : in  std_logic;
+		  signal out_full      : in  std_logic;
+		  signal in_rho        : in  std_logic_vector (RHO_WIDTH-1  downto 0);
+		  signal in_theta      : in  std_logic_vector (7 downto 0);
+		  signal in_rho_done   : in  std_logic;
+		  signal in_rd_en      : out std_logic;
+		  signal out_wr_en     : out std_logic;
+		  signal out_xy_done   : out std_logic;
+		  signal x1            : out std_logic_vector (RHO_WIDTH-1 downto 0);
+		  signal x2            : out std_logic_vector (RHO_WIDTH-1  downto 0);
+		  signal y1            : out std_logic_vector (RHO_WIDTH-1  downto 0);
+		  signal y2            : out std_logic_vector (RHO_WIDTH-1  downto 0)
+	  );
+	end component finite_lines;
+
+	component draw_lines is
+		port (
+			signal clock			 			     	: in std_logic;
+			signal reset			 			     	: in std_logic;
+			signal x1   	     			      : in std_logic_vector (RHO_WIDTH - 1 downto 0);
+			signal x2 	       			      : in std_logic_vector (RHO_WIDTH - 1 downto 0);
+			signal y1	        			      : in std_logic_vector (RHO_WIDTH - 1 downto 0);
+			signal y2        			      	: in std_logic_vector (RHO_WIDTH - 1 downto 0);
+			signal xy_in_empty				    : in std_logic;  -- 0: input data available; 1: no data available
+			signal xy_done					      : in std_logic;  -- xy coordinates calculation done, all lines have been calculated.
+			signal in_rd_en					      : out std_logic; -- 0: not ready to read, 1: read enable
+			signal in_empty	 				     	: in std_logic;  -- 0: input data available; 1: no data available
+			signal accumulator_rd_en      : in std_logic;  -- Need to lineup with Hough block, not reading every cycle
+			signal in_dout		      			: in std_logic_vector (MAG_WIDTH - 1 downto 0); -- accumulator input
+			signal out_wr_en	  			    : out std_logic; -- 0: output data not valid; 1: valid output
+			signal out_full		 			 	    : in std_logic;  -- 0: output buffer not full, 1: output buffer full (PAUSE)
+			signal out_din	   			  	  : out std_logic_vector (MAG_WIDTH - 1 downto 0)
+		);
+	end component draw_lines;
+
+	-- Supporting Components
+
+	component cordic_stage is
+	port (
+		i : in integer;
+		atan : in std_logic_vector (15 downto 0);
+		x_i : in std_logic_vector (31 downto 0);
+		y_i : in std_logic_vector (31 downto 0);
+		z_i : in std_logic_vector (31 downto 0);
+		x_o : out std_logic_vector (31 downto 0);
+		y_o : out std_logic_vector (31 downto 0);
+		z_o : out std_logic_vector (31 downto 0)
+	);
+	end component cordic_stage;
+
+	component fifo is
+		generic	(
+			constant FIFO_DATA_WIDTH : integer := 32;
+			constant FIFO_BUFFER_SIZE : integer := 16
+		);
+		port (
+			signal rd_clk : in std_logic;
+			signal wr_clk : in std_logic;
+			signal reset : in std_logic;
+			signal rd_en : in std_logic;
+			signal wr_en : in std_logic;
+			signal din : in std_logic_vector ((FIFO_DATA_WIDTH - 1) downto 0);
+			signal dout : out std_logic_vector ((FIFO_DATA_WIDTH - 1) downto 0);
+			signal full : out std_logic;
+			signal empty : out std_logic
+		);
+	end component fifo;
+
+	component cordic is
+		port (
+			clock : in std_logic;
+			reset : in std_logic;
+			in_dout : in std_logic_vector (31 downto 0);
+			in_rd_en : out std_logic;
+			in_empty : in std_logic;
+			out_cos_wr_en : out std_logic;
+			out_cos_din : out std_logic_vector (31 downto 0);
+			out_cos_full : in std_logic;
+			out_sin_wr_en : out std_logic;
+			out_sin_din : out std_logic_vector (31 downto 0);
+			out_sin_full : in std_logic
+		);
+	end component cordic;
 
 end package;
--- HoughTransform/threshold.vhd
+++ HoughTransform/threshold.vhd
@@ -5,21 +5,22 @@ use IEEE.numeric_std.all;
 -- use IEEE.numeric_std_unsigned;
 use work.hough_constants.all;
 
-entity hough_threshold is
+entity threshold is
   port (
-    signal clock		  : in std_logic;
-    signal reset		  : in std_logic;
-    signal in_rd_en		: out std_logic;
-    signal in_empty		: in std_logic;
-    signal in_dout		: in std_logic_vector (MAG_WIDTH - 1 downto 0); -- accumulator input
-    signal out_wr_en	: out std_logic;
-    signal out_full		: in std_logic;
-    signal out_rho	  : out std_logic_vector (RHO_WIDTH - 1 downto 0);
-    signal out_theta	: out std_logic_vector (THETA_WIDTH - 1 downto 0)
-    );
-end entity hough_threshold;
+    signal clock	       	: in std_logic;
+    signal reset	    	  : in std_logic;
+    signal in_rd_en	    	: out std_logic;
+    signal in_empty	    	: in std_logic;
+    signal out_full	    	: in std_logic;
+    signal in_dout		    : in std_logic_vector (MAG_WIDTH - 1 downto 0); -- accumulator input
+    signal out_wr_en	    : out std_logic;  -- '1' when out_rho and out_theta are valid
+    signal out_rho_done   : out std_logic;  -- RHO/THETA calculation done
+    signal out_rho	      : out std_logic_vector (RHO_WIDTH - 1 downto 0);
+    signal out_theta	    : out std_logic_vector (THETA_WIDTH - 1 downto 0)
+  );
+end entity threshold;
 
-architecture behavioral of hough_threshold is
+architecture behavioral of threshold is
 
   constant REG_SIZE   : integer := (ACCU_WIDTH * 6) + 7; -- size of shift register,7x7 window
 
@@ -33,9 +34,16 @@ architecture behavioral of hough_threshold is
 
   signal x, x_c       : integer; -- x-coordinate
   signal y, y_c       : integer; -- y-coordinate
+  signal rho          : integer;
+  signal theta        : integer;
 
+
+  signal data_valid    : std_logic;
+  signal rho_done_c    : std_logic;
+  signal rho_done      : std_logic;
   signal accu_data      : integer;
 
+  -- Use 7x7 window to do noise filtering, coordinate is only valid if it's local maximum
   function h_threshold_op (data : ARRAY_SLV(0 to 48)) return std_logic is
     variable accu_00		: integer;
     variable accu_01		: integer;
@@ -61,8 +69,9 @@ architecture behavioral of hough_threshold is
     variable accu_21		: integer;
     variable accu_22		: integer;
     variable accu_23		: integer;
-    variable current_accu  : integer;  -- pixel 24 is the current
+    variable current_accu           : integer;  -- pixel 24 is the current
     -- variable accu_24		: integer;
+    -- 7x7
     variable accu_25		: integer;
     variable accu_26		: integer;
     variable accu_27		: integer;
@@ -87,9 +96,11 @@ architecture behavioral of hough_threshold is
     variable accu_46		: integer;
     variable accu_47		: integer;
     variable accu_48		: integer;
-
+    --
     variable local_max_1		: std_logic;
     variable local_max_2		: std_logic;
+    variable zero		        : std_logic := '0';
+    variable one		        : std_logic := '1';
 
   begin
     -- previous rows
@@ -117,7 +128,7 @@ architecture behavioral of hough_threshold is
     accu_21  := to_integer(unsigned(data(21)));
     accu_22  := to_integer(unsigned(data(22)));
     accu_23  := to_integer(unsigned(data(23)));
-                --
+    --
     current_accu := to_integer(unsigned(data(24)));
     -- next rows
     accu_25  := to_integer(unsigned(data(25)));
@@ -145,76 +156,74 @@ architecture behavioral of hough_threshold is
     accu_47  := to_integer(unsigned(data(47)));
     accu_48  := to_integer(unsigned(data(48)));
 
-    -- Only return valid coordinate (rho,theta) if current_acc > THRESHOLD and is local maximum.
-    -- The valid coordinate (rho,theta) will be converted back to two points (x1,y1) and (x2,t2) in image space.
-    -- 7x7 window, check 49 values
+    -- Only return valid coordinate (r,Theta) if current_acc > THRESHOLD and is local maximum.
+    -- The valid coordinate (r,Theta) will be converted back to two points (X1,Y1) and (X2,Y2) in image space.
+    -- 7x7 neighbor window, check 49 values
     -- Check the first 24
-      if ((accu_00 > current_accu) or (accu_01  > current_accu) or (accu_02  > current_accu)  or (accu_03  > current_accu) or (accu_04  > current_accu)
-      or (accu_05  > current_accu) or (accu_06  > current_accu) or (accu_07  > current_accu) or (accu_08  > current_accu) or (accu_09  > current_accu)
-      or (accu_10  > current_accu) or (accu_11  > current_accu) or (accu_12  > current_accu) or (accu_13  > current_accu) or (accu_14  > current_accu)
-      or (accu_15  > current_accu) or (accu_16  > current_accu) or (accu_17  > current_accu) or (accu_18  > current_accu) or (accu_19  > current_accu)
-      or (accu_20  > current_accu) or (accu_21  > current_accu) or (accu_22  > current_accu) or (accu_23  > current_accu)) then
-        local_max_1 := '0';
-      else
-        local_max_1 := '1';
-      end if;
-
+    if ((accu_00 > current_accu) or (accu_01  > current_accu) or (accu_02  > current_accu)  or (accu_03  > current_accu) or (accu_04  > current_accu)
+    or (accu_05  > current_accu) or (accu_06  > current_accu) or (accu_07  > current_accu) or (accu_08  > current_accu) or (accu_09  > current_accu)
+    or (accu_10  > current_accu) or (accu_11  > current_accu) or (accu_12  > current_accu) or (accu_13  > current_accu) or (accu_14  > current_accu)
+    or (accu_15  > current_accu) or (accu_16  > current_accu) or (accu_17  > current_accu) or (accu_18  > current_accu) or (accu_19  > current_accu)
+    or (accu_20  > current_accu) or (accu_21  > current_accu) or (accu_22  > current_accu) or (accu_23  > current_accu)) then
+      local_max_1 := '0';
+    else
+      local_max_1 := '1';
+    end if;
     -- Check the second set of 24
-      if ((accu_25 > current_accu) or (accu_26  > current_accu) or (accu_27  > current_accu)  or (accu_28  > current_accu) or (accu_29  > current_accu)
-      or (accu_30  > current_accu) or (accu_31  > current_accu) or (accu_32  > current_accu) or (accu_33  > current_accu) or (accu_34  > current_accu)
-      or (accu_35  > current_accu) or (accu_36  > current_accu) or (accu_37  > current_accu) or (accu_38  > current_accu) or (accu_39  > current_accu)
-      or (accu_40  > current_accu) or (accu_41  > current_accu) or (accu_42  > current_accu) or (accu_43  > current_accu) or (accu_44  > current_accu)
-      or (accu_45  > current_accu) or (accu_46  > current_accu) or (accu_47  > current_accu) or (accu_48  > current_accu)) then
-        local_max_2 := '0';
-      else
-        local_max_2 := '1';
-      end if;
+    if ((accu_25 > current_accu) or (accu_26  > current_accu) or (accu_27  > current_accu)  or (accu_28  > current_accu) or (accu_29  > current_accu)
+    or (accu_30  > current_accu) or (accu_31  > current_accu) or (accu_32  > current_accu) or (accu_33  > current_accu) or (accu_34  > current_accu)
+    or (accu_35  > current_accu) or (accu_36  > current_accu) or (accu_37  > current_accu) or (accu_38  > current_accu) or (accu_39  > current_accu)
+    or (accu_40  > current_accu) or (accu_41  > current_accu) or (accu_42  > current_accu) or (accu_43  > current_accu) or (accu_44  > current_accu)
+    or (accu_45  > current_accu) or (accu_46  > current_accu) or (accu_47  > current_accu) or (accu_48  > current_accu)) then
+      local_max_2 := '0';
+    else
+      local_max_2 := '1';
+    end if;
 
-      if ((current_accu > ACCU_THRESHOLD) and (local_max_1 = '1') and (local_max_2 = '1')) then
-        return '1';
-      else
-        return '0';
-      end if;
+    if ((current_accu > ACCU_THRESHOLD) and (local_max_1 = '1') and (local_max_2 = '1')) then
+      return one;
+    else
+      return zero;
+    end if;
 
   end h_threshold_op;
 
 begin
 
+  out_rho_done <= rho_done;
 
   h_thresh_process : process(state, in_empty, in_dout, out_full, x, y, shift_reg) is
-    variable data		         : ARRAY_SLV (0 to 48);
-    variable coord_valid     : std_logic;
-    variable rho             : integer;
-    variable theta           : integer;
+    variable data		       : ARRAY_SLV (0 to 48);
+    variable coord_valid   : std_logic;
   begin
     next_state <= state;
     x_c <= x;
     y_c <= y;
     shift_reg_c <= shift_reg;
     in_rd_en    <= '0';
-    out_wr_en   <= '0';
-    out_rho     <= (others => '0');  -- Default output value
-    out_theta   <= (others => '0');  -- Default output value
+    rho_done_c  <= rho_done;
 
     -- 7x7 window
-    for i in 0 to 6 loop -- row
-      for j in 0 to 6 loop -- column
+    for i in 0 to 6 loop                 -- row
+      for j in 0 to 6 loop         -- column
         data(i*7+j) := shift_reg (i*ACCU_WIDTH + j);
       end loop;
     end loop;
 
     coord_valid := '0';
-    rho := 0;
-    theta := 0;
     accu_data   <= to_integer(unsigned(data(24)));  -- current_pixel, debug purpose
     if ( (x /= 0) AND (x /= ACCU_WIDTH-1) AND (y /= 0) AND (y /= ACCU_HEIGHT-1) ) then
       coord_valid := h_threshold_op (data);
       if (coord_valid = '1')  then
-        rho := y;
-        theta := x;
+        rho     <= y-3;   -- 3 rows and column offset due to 7x7 window
+        theta   <= x-3;
+      else
+        rho     <= 0;
+        theta   <= 0;
       end if;
     end if;
 
+    data_valid <= coord_valid;
     case (state) is
       when s0 =>
         x_c <= 0;
@@ -222,25 +231,13 @@ begin
         shift_reg_c <= (others => (others => '0'));
         next_state <= s1;
 
-      when s1 =>
+      when s1 =>      -- 1st row
         if (in_empty = '0') then
+          rho_done_c  <= '0';
           in_rd_en <= '1';
           shift_reg_c(0 to REG_SIZE-2) <= shift_reg(1 to REG_SIZE-1);
           shift_reg_c(REG_SIZE-1) <= in_dout;
-          x_c <= x + 1;
-          if ( x = ACCU_WIDTH - 1 ) then
-            x_c <= 0;
-            y_c <= y + 1;
-          end if;
-
-          --if ( (y * ACCU_WIDTH + x) = (ACCU_WIDTH + 1) ) then
-          --  x_c <= x - 1;
-          --  y_c <= y - 1;
-          if (y = 3 and x = 3) then
-            x_c <= x - 3;
-            y_c <= y - 3;
-            next_state <= s2;
-          end if;
+          next_state <= s2;
         end if;
 
       when s2 =>
@@ -253,10 +250,8 @@ begin
             y_c <= y + 1;
           end if;
 
-          out_wr_en <= coord_valid;
-          out_rho   <= std_logic_vector(to_unsigned(rho, RHO_WIDTH));
-          out_theta <= std_logic_vector(to_unsigned(theta, THETA_WIDTH));
           in_rd_en <= '1';
+
           if ( y = ACCU_HEIGHT-2 AND x = ACCU_WIDTH-3 ) then
             next_state <= s3;
           else
@@ -264,7 +259,7 @@ begin
           end if;
         end if;
 
-      when s3 =>
+      when s3 =>     -- last row
         if ( out_full = '0' ) then
           x_c <= x + 1;
           if ( x = ACCU_WIDTH - 1 ) then
@@ -272,11 +267,9 @@ begin
             y_c <= y + 1;
           end if;
 
-          out_rho   <= std_logic_vector(to_unsigned(rho,RHO_WIDTH));
-          out_theta <= std_logic_vector(to_unsigned(theta,THETA_WIDTH));
-          out_wr_en <= coord_valid;
           if ( (x = ACCU_WIDTH-1) AND (y = ACCU_HEIGHT-1) ) then
-            next_state <= s0;
+            next_state  <= s0;
+            rho_done_c  <= '1';
           end if;
         end if;
 
@@ -295,11 +288,21 @@ begin
       x <= 0;
       y <= 0;
       shift_reg <= (others => (others => '0'));
+      out_rho   <= (others => '0');
+      out_theta <= (others => '0');
+      rho_done  <= '0';
+      out_wr_en <= '0';
     elsif (rising_edge(clock)) then
-      state <= next_state;
+      state         <= next_state;
+      rho_done      <= rho_done_c;
       x <= x_c;
       y <= y_c;
       shift_reg <= shift_reg_c;
+      out_wr_en <= data_valid;
+      if (data_valid = '1') then
+        out_rho   <= std_logic_vector(to_unsigned(rho,RHO_WIDTH));
+        out_theta <= std_logic_vector(to_unsigned(theta,THETA_WIDTH));
+      end if;
     end if;
   end process clock_process;
 
